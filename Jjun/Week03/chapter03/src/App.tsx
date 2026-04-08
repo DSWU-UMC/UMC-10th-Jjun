7de6d403980c1a0b00b74f378af3f0e2f63dcff7
june <glass040604@gmail.com>
2026-04-09 02:06:21 +0900

    chapter03 수정

--- Jjun/Week03/chapter03/src/App.tsx
+++ Jjun/Week03/chapter03/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Movie, MovieResponse } from './types/movie';
+import type { Movie, MovieResponse } from './types/movie';
 import axios from 'axios';
 
 const MoviesPage = () => {
@@ -7,15 +7,19 @@ const MoviesPage = () => {
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const { data } = await axios.get<MovieResponse>(
-        'https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1',
-        {
-          headers: {
-            Authorization: `Bearer 토큰값`, // 본인 TMDB 토큰으로 교체
-          },
-        }
-      );
-      setMovies(data.results);
+      try {
+        const { data } = await axios.get<MovieResponse>(
+          'https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1',
+          {
+            headers: {
+              Authorization: `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmFmYjFlMzU2NTYwMGY0ZmQzYTA2NzkzZDU0MjcwYSIsIm5iZiI6MTc3NTAwNzQ1MS40NDcsInN1YiI6IjY5Y2M3NmRiMmE2MmE5MjZmZjQ4ZWQyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Zp49zOmO-mzc0rDf8RuVCuXjL_DeaZoPPSlUAh38yvE`,
+            },  
+          }
+        );
+        setMovies(data.results);
+      } catch (error) {
+        console.error('영화 데이터 조회 실패', error);
+      }
     };
 
     fetchMovies();
